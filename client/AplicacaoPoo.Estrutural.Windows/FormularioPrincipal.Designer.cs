613f1ddf9cd2dc8f6a0e0ebcbb013e66710019af
Vilany Brito <vilany.britto@gmail.com>
2022-09-15 22:00:39 -0300

    Inicio da AplicacaoPoo.Estrutural.Windows

---
+++ client/AplicacaoPoo.Estrutural.Windows/FormularioPrincipal.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace AplicacaoPoo.Estrutural.Windows
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCotacaoDoDolar = new System.Windows.Forms.Button();
+            this.btnControleVendedor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCotacaoDoDolar
+            // 
+            this.btnCotacaoDoDolar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnCotacaoDoDolar.Location = new System.Drawing.Point(48, 58);
+            this.btnCotacaoDoDolar.Name = "btnCotacaoDoDolar";
+            this.btnCotacaoDoDolar.Size = new System.Drawing.Size(116, 23);
+            this.btnCotacaoDoDolar.TabIndex = 0;
+            this.btnCotacaoDoDolar.Text = "Cotação do dólar";
+            this.btnCotacaoDoDolar.UseVisualStyleBackColor = false;
+            this.btnCotacaoDoDolar.Click += new System.EventHandler(this.cotacaoDoDolar);
+            // 
+            // btnControleVendedor
+            // 
+            this.btnControleVendedor.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnControleVendedor.Location = new System.Drawing.Point(215, 58);
+            this.btnControleVendedor.Name = "btnControleVendedor";
+            this.btnControleVendedor.Size = new System.Drawing.Size(120, 23);
+            this.btnControleVendedor.TabIndex = 1;
+            this.btnControleVendedor.Text = "Controle Vendedor";
+            this.btnControleVendedor.UseVisualStyleBackColor = false;
+            this.btnControleVendedor.Click += new System.EventHandler(this.btnControleVendedor_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(405, 136);
+            this.Controls.Add(this.btnControleVendedor);
+            this.Controls.Add(this.btnCotacaoDoDolar);
+            this.Name = "frmPrincipal";
+            this.Text = "frmPrincipal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnCotacaoDoDolar;
+        private Button btnControleVendedor;
+    }
+}
---
+++ client/AplicacaoPoo.Estrutural.Windows/cotacaoDolar.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AplicacaoPoo.Estrutural.Windows
+{
+    partial class frmCotacaoDoDolar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCotacaoDolar = new System.Windows.Forms.Label();
+            this.lbllerValorDolar = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCotacaoDolar
+            // 
+            this.lblCotacaoDolar.AutoSize = true;
+            this.lblCotacaoDolar.Location = new System.Drawing.Point(74, 101);
+            this.lblCotacaoDolar.Name = "lblCotacaoDolar";
+            this.lblCotacaoDolar.Size = new System.Drawing.Size(98, 15);
+            this.lblCotacaoDolar.TabIndex = 0;
+            this.lblCotacaoDolar.Text = "Cotação do dólar";
+            // 
+            // lbllerValorDolar
+            // 
+            this.lbllerValorDolar.AccessibleRole = System.Windows.Forms.AccessibleRole.WhiteSpace;
+            this.lbllerValorDolar.AutoSize = true;
+            this.lbllerValorDolar.Location = new System.Drawing.Point(90, 144);
+            this.lbllerValorDolar.Name = "lbllerValorDolar";
+            this.lbllerValorDolar.Size = new System.Drawing.Size(64, 15);
+            this.lbllerValorDolar.TabIndex = 1;
+            this.lbllerValorDolar.Text = "Valor Dolar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(99, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Conversor de valor";
+            // 
+            // frmCotacaoDoDolar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbllerValorDolar);
+            this.Controls.Add(this.lblCotacaoDolar);
+            this.Name = "frmCotacaoDoDolar";
+            this.Text = "frmCotacaoDoDolar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblCotacaoDolar;
+        private Label lbllerValorDolar;
+        private Label label3;
+    }
+}
